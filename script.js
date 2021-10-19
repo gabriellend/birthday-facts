5d4511cf862100ad07081aba4a323c67a63d8c2b
Gabrielle Davidson <gdvdsn@gmail.com>
2021-10-18 17:40:57 -1000

    Added birthdate and optimized for different screen sizes

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ let paragraph = document.getElementById('paragraph');
 let monthInput = document.getElementById('month');
 let dayInput = document.getElementById('day');
 let tooltip = document.getElementsByClassName('tooltip')[0];
+let birthday = document.getElementById('birthday');
 
-const displayFact = (fact) => {
+const displayFact = (fullFact) => {
+  let factArray = fullFact.split(' ');
+  let birthdate = factArray.splice(0, 2).join(' ');
+  let fact = factArray.splice(1).join(' ');
+
+  birthday.innerText = birthdate;
   paragraph.innerText = fact;
 }
 
@@ -26,7 +32,7 @@ const fetchFact = async (event) => {
   let day = dayInput.value;
   let response = await fetch(`http://numbersapi.com/${month}/${day}/date?json`);
   let data = await response.json();
-  
+
   displayFact(data.text);
 }
 

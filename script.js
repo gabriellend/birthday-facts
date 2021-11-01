36fe93a7156b904916f30475dcf9b50ae8a6fe65
Gabrielle Davidson <gdvdsn@gmail.com>
2021-10-31 19:45:54 -1000

    Provided a message in case invalid dates are entered

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let dayInput = document.getElementById('day');
 let tooltip = document.getElementsByClassName('tooltip')[0];
 let birthday = document.getElementById('birthday');
 
+
 const displayFact = (fullFact) => {
   let factArray = fullFact.split(' ');
   let birthdate = factArray.splice(0, 2).join(' ');
@@ -13,27 +14,42 @@ const displayFact = (fullFact) => {
   paragraph.innerText = fact;
 }
 
+
 const toggleTooltip = () => {
   tooltip.classList.toggle('toggle-tooltip');
 }
+
+
+const validateBirthday = (month, day) => {
+  if (month > 0 && month <= 12 && day > 0 && day <= 31) {
+    return true;
+  }
+}
+
 
 let callCount = 0;
 
 const fetchFact = async (event) => {
   event.preventDefault();
-
-  if (callCount < 2) {
-    toggleTooltip();
-  }
-
-  callCount++;
    
   let month = monthInput.value;
   let day = dayInput.value;
-  let response = await fetch(`https://number-api-swart.vercel.app/fact/${month}/${day}`);
-  let data = await response.json();
 
-  displayFact(data.text);
+  if (validateBirthday(month, day)) {
+    callCount++;
+    if (callCount < 3) {
+      toggleTooltip();
+    }
+
+    let response = await fetch(`https://number-api-swart.vercel.app/fact/${month}/${day}`);
+    let data = await response.json();
+      
+    displayFact(data.text);
+  } else {
+    birthday.innerText = "";
+    paragraph.innerText = "So sorry, that's not a valid date D:";
+  }
+
 }
 
 
